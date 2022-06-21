a07640f83ed25df87e8d4210c8fa8c2cd57bebb3
Griff-Thaxter <sniffinisakuntt@gmail.com>
2022-06-21 13:24:04 -0600

    Part D Complete

--- src/java/services/RoleService.java
+++ src/java/services/RoleService.java
@@ -1,5 +1,13 @@
 package services;
 
+import dataaccess.RoleDB;
+import java.util.List;
+import models.Role;
+
 public class RoleService {
-    
+    public List<Role> getAll(int roleID) throws Exception {
+        RoleDB roleDB = new RoleDB();
+        List<Role> roles = roleDB.getAll(roleID);
+        return roles;
+    }
 }
--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -1,5 +1,38 @@
 package services;
 
+import dataaccess.UserDB;
+import java.util.List;
+import models.User;
+
 public class UserService {
+    public User get(String email) throws Exception {
+        UserDB userDB = new UserDB();
+        User user = userDB.get(email);
+        return user;
+    }
     
+    public List<User> getAll(String email) throws Exception {
+        UserDB userDB = new UserDB();
+        List<User> users = userDB.getAll(email);
+        return users;
+    }
+    
+    public void insert(boolean active, String firstName, String lastName, String password, String role) throws Exception {
+        User user = new User(null, active, firstName, lastName, password, role);
+        UserDB userDB = new UserDB();
+        userDB.insert(user);
+    }
+    
+    public void update(String email, boolean active, String firstName, String lastName, String password, String role) throws Exception {
+        User user = new User(email, active, firstName, lastName, password, role);
+        UserDB userDB = new UserDB();
+        userDB.update(user);
+    }
+    
+    public void delete(String email) throws Exception {
+        User user = new User();
+        user.setEmail(email);
+        UserDB userDB = new UserDB();
+        userDB.delete(user);
+    }
 }
